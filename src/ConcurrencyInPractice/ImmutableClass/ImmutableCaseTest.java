4b17a3a2a1748ade9de46f8c090e17106100a2c0
frankie <lyf.email@qq.com>
2023-02-11 18:36:48 +0800

    Immutable Test

---
+++ src/ConcurrencyInPractice/ImmutableClass/ImmutableCaseTest.java
@@ -0,0 +1,63 @@
+package ConcurrencyInPractice.ImmutableClass;
+
+import java.util.Arrays;
+
+public class ImmutableCaseTest {
+
+    public static void main(String[] args) {
+        UnsafeStates unsafeStates = new UnsafeStates();
+        writeStates(unsafeStates);
+        readStates(unsafeStates);
+
+        SafteStates safeStates = new SafteStates();
+        writeStates(safeStates);
+        readStates(safeStates);
+    }
+
+    private static void writeStates(States states){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                states.getStates()[0] = "AAA";
+            }
+        });
+
+        thread.start();
+    }
+
+    private static void readStates(States states){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if("AK".equals(states.getStates()[0])){
+                    System.out.println(states.getSateName() + ": Everything is good");
+                }else{
+                    System.out.println(states.getSateName() + ": Something is bad");
+                }
+            }
+        });
+
+        thread.start();
+    }
+}
+
+interface States{
+    public String getSateName();
+    public String[] getStates();
+}
+class UnsafeStates implements States{
+    private String[] states = new String[]{"AK", "AL"};
+    public String getSateName(){
+        return "UnSafeStates";
+    }
+    public String[] getStates(){return states;};
+}
+
+class SafteStates implements States{
+    private final String[] states = new String[]{"AK", "AL"};
+    public String getSateName(){
+        return "SafeStates";
+    }
+    public String[] getStates(){ return (String[])Arrays.copyOf(states, states.length); }
+}
+
