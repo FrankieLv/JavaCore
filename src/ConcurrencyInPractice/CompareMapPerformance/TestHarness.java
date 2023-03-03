10a8b4eca293f8b312d4e0edf16ffb509f4016d1
FrankieLv <32904224+FrankieLv@users.noreply.github.com>
2023-03-03 09:28:50 +0800

    Create TestHarness.java

---
+++ src/ConcurrencyInPractice/CompareMapPerformance/TestHarness.java
@@ -0,0 +1,33 @@
+package ConcurrencyInPractice.CompareMapPerformance;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestHarness {
+    public long timeTasks(int threadCount, final Runnable task) throws InterruptedException {
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(threadCount);
+
+        for(int i=0;i<threadCount;i++) {
+            Thread t = new Thread() {
+                public void run() {
+                    try{
+                        startGate.await();
+                        try{
+                            task.run();
+                        }finally {
+                            endGate.countDown();
+                        }
+                    }catch (InterruptedException ignore) {}
+                }
+            };
+            t.start();
+        }
+
+        long startTime = System.nanoTime();
+        startGate.countDown();
+        endGate.await();
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+}
